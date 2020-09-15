179f7761f92a51bde498f8e0754a629c192f2269
LIimTaeGeon <gentle1998@naver.com>
2020-09-15 07:42:15 +0000

    9/15 error heart & bookmark

--- post/templatetags/post_extras.py
+++ post/templatetags/post_extras.py
@@ -1,7 +1,7 @@
 from django import template
 import re
 
-regitser = tmeplate.Library()
+register = template.Library()
 
 @register.filter
 def add_link(value):
@@ -9,5 +9,5 @@ def add_link(value):
     tags = value.tag_set.all()
     for tag in tags:
         # content = re.sub(patterns, reql, conent)
-        content = re.sub(r'\#' + tag.name + r'\b', '<a href="/post/explore/tags/' + tag.name '">#' + tag.name'</a>', content)
+        content = re.sub(r'\#'+tag.name+r'\b', '<a href="/post/explore/tags/'+tag.name+'">#'+tag.name+'</a>', content)
     return content
