772153d1c1ddd6c5d403053a6b2ce09a3fc6f104
LIimTaeGeon <gentle1998@naver.com>
2020-09-18 06:50:22 +0000

    9/18

--- post/views.py
+++ post/views.py
@@ -15,24 +15,24 @@ from django.db.models import Count
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     comment_form = CommentForm()
-    
+
     return render(request, 'post/post_detail.html', {
         'comment_form': comment_form,
         'post': post,
     })
-    
+
 @login_required
 def my_post_list(request, username):
     user = get_object_or_404(get_user_model(), username=username)
     user_profile = user.profile
-    
+
     target_user = get_user_model().objects.filter(id=user.id).select_related('profile') \
         .prefetch_related('profile__follower_user__from_user', 'profile__follow_user__to_user')
-        
+
     post_list = user.post_set.all()
-    
+
     all_post_list = Post.objects.all()
-    
+
     return render(request, 'post/my_post_list.html', {
         'user_profile': user_profile,
         'target_user': target_user,
@@ -43,7 +43,7 @@ def my_post_list(request, username):
 
 def post_list(request, tag=None):
     tag_all = Tag.objects.annotate(num_post=Count('post')).order_by('-num_post')
-    
+
     if tag:
         post_list = Post.objects.filter(tag_set__name__iexact=tag) \
             .prefetch_related('tag_set', 'like_user_set__profile', 'comment_set__author__profile',
@@ -56,35 +56,35 @@ def post_list(request, tag=None):
             .select_related('author__profile')
 
     comment_form = CommentForm()
-    
+
     paginator = Paginator(post_list, 3)
     page_num = request.POST.get('page')
-    
+
     try:
         posts = paginator.page(page_num)
     except PageNotAnInteger:
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-        
+
     if request.is_ajax():
         return render(request, 'post/post_list_ajax.html', {
             'posts': posts,
             'comment_form': comment_form,
         })
-    
+
     if request.method == 'POST':
         tag = request.POST.get('tag')
         tag_clean = ''.join(e for e in tag if e.isalnum())
         return redirect('post:post_search', tag_clean)
-    
+
     if request.user.is_authenticated:
         username = request.user
         user = get_object_or_404(get_user_model(), username=username)
         user_profile = user.profile
         follow_set = request.user.profile.get_following
         follow_post_list = Post.objects.filter(author__profile__in=follow_set)
-        
+
         return render(request, 'post/post_list.html', {
             'user_profile': user_profile,
             'tag': tag,
@@ -100,7 +100,7 @@ def post_list(request, tag=None):
             'comment_form': comment_form,
             'tag_all': tag_all,
         })
-    
+
 @login_required
 def post_new(request):
     if request.method == 'POST':
@@ -124,7 +124,7 @@ def post_edit(request, pk):
     if post.author != request.user:
         messages.warning(request, '잘못된 접근입니다')
         return redirect('post:post_list')
-    
+
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
@@ -139,7 +139,7 @@ def post_edit(request, pk):
         'post': post,
         'form': form,
     })
-    
+
 @login_required
 @require_POST
 def post_like(request):
@@ -200,7 +200,7 @@ def comment_new(request):
             comment.post = post
             comment.save()
             return render(request, 'post/comment_new_ajax.html', {
-                'comment': comment,   
+                'comment': comment,
             })
     return redirect("post:post_list")
 
@@ -230,5 +230,5 @@ def comment_delete(request):
     else:
         message = '잘못된 접근입니다'
         status = 0
-        
+
     return HttpResponse(json.dumps({'message': message, 'status': status, }), content_type="application/json")
